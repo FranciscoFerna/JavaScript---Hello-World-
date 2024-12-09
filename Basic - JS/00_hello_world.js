2674870523bb7bd85ec98dc7156f4a037a9ace55
Francisco Fernandez <fraanfeernaandeez@gmail.com>
2024-12-09 12:05:47 +0100

    00_hello_world COMPLETE

---
+++ Basic - JS/00_hello_world.js
@@ -0,0 +1,29 @@
+/*
+@author: Francisco Fernández Navarro
+#date: 9 de Diciembre de 2024
+
+file '00_hello_world'
+*/
+
+// Comentario simple '//'
+
+/*
+Esto es
+un comentario
+de bloque
+*/
+
+console.log("Hello, JavaScript!")
+console.log('Hello, JavaScript!')
+console.log(`Hello, JavaScript!`)
+
+// -> Comun: console.log("Hello JavaScript!")
+
+console.log("9")
+console.log(9)
+console.log(7 + 3)
+console.log(7 - 3)
+console.log(7 * 3)
+console.log(7 / 3)
+console.log(7 % 3)
+console.log(7 ** 3)
