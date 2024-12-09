8bfd2a7581356af2325e2c32ac2dfb672f943957
Francisco Fernandez <fraanfeernaandeez@gmail.com>
2024-12-09 12:31:17 +0100

    02_datatypes COMPLETE

---
+++ Basic - JS/02_datatypes.js
@@ -0,0 +1,53 @@
+/*
+@author: Francisco Fernández Navarro
+#date: 9 de Diciembre de 2024
+
+file '02_datatypes'
+*/
+
+// Cadenas de texto (string)
+let myName = "Francisco Fernández"
+let myAlias = 'Franxu'
+let myEmail = `fraanfeernaandeez@gmail.com`
+
+// Numeros (number)
+let age = 18
+let height = 1.78 // Decimal
+
+// Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(817245671876987986589716389471628379612983761289376129)
+let myBigIntN = 817245671876987986589716389471628379612983761289376129n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof myAliasalias)
+console.log(typeof myEmail)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigIntN)
