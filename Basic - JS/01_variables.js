1951eda952ecb8bf457322a20b78020063b45362
Francisco Fernandez <fraanfeernaandeez@gmail.com>
2024-12-09 12:14:30 +0100

    01_variables COMPLETADO

---
+++ Basic - JS/01_variables.js
@@ -0,0 +1,32 @@
+/*
+@author: Francisco Fernández Navarro
+#date: 9 de Diciembre de 2024
+
+file '01_variables'
+*/
+
+// var
+
+var helloJavaScript = "Hola, JavaScript!"
+console.log(helloJavaScript)
+
+helloJavaScript = "Hola otra vez, JavaScript!"
+console.log(helloJavaScript)
+
+// let
+let helloJavaScript2 = "Hola, JavaScript 2!"
+console.log(helloJavaScript2)
+
+helloJavaScript2 = "Hola otra vez, JavaScript 2!"
+console.log(helloJavaScript2)
+
+// const
+const helloJavaScript3 = "Hola, JavaScript 3!"
+console.log(helloJavaScript)
+
+helloJavaScript3 = "Hola otra vez, JavaScript 3!"
+console.log(helloJavaScript)
+
+// ERROR
+// helloJavaScript3 = "Hola otra vez, JavaScript 3!"
+// console.log(helloJavaScript)
